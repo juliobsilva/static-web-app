dd066a24ca96b178e4b2b2aa81430cbc6272f5a7
Julio César Barbosa da Silva <j.barbosa.da.silva@avanade.com>
2024-07-09 11:21:25 -0300

    fix

---
+++ static/script.js
@@ -0,0 +1,4 @@
+// Função para mostrar uma mensagem de boas-vindas no console
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('Bem-vindo ao Meu Site Estático em HTML-LAB');
+});
